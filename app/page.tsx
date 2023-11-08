e4a8ffdbf6469c49fdd6329d9c74a5dd21bd663e
devops3safelab <devops3safelabs@gmail.com>
2023-11-08 17:19:22 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Stepper from '@/components/Stepper'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="h-screen w-screen flex justify-center items-center h-scree">
+      <Stepper />
+    </main>
+  )
+}
